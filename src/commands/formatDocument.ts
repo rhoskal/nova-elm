8f8771b557c21794732a7fea4bb06cfea2d08c5f
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-07-15 18:39:11 -0400

    remove ts code for now

--- src/commands/formatDocument.ts
+++
@@ -1,1 +0,0 @@
-export const registerFormatDocument = (client: LanguageClient): void => {};
--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export enum ExtensionConfigKeys {
-  ElmPath = "hansjhoffman.elm.config.elmPath",
-  ElmFormatPath = "hansjhoffman.elm.config.elmFormatPath",
-  ElmReviewPath = "hansjhoffman.elm.config.elmReviewPath",
-  ElmTestPath = "hansjhoffman.elm.config.elmTestPath",
-  LSDisableDiagnostics = "hansjhoffman.elm.config.disableDiagnostics",
-  LSReviewDiagnostics = "hansjhoffman.elm.config.elmReviewDiagnostics",
-  LSTrace = "hansjhoffman.elm.config.elmTrace",
-}
